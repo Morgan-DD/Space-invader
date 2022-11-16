70d276b5b764454f8a02f0684a4bbc4565a96ebb
Morgan <111990474+Morgan-DD@users.noreply.github.com>
2022-11-16 10:26:52 +0100

    -

--- code/SpaceInvader/SpaceInvader/Program.cs
+++ code/SpaceInvader/SpaceInvader/Program.cs
@@ -20,7 +20,7 @@ namespace SpaceInvader
         static void Main(string[] args)
         {
             bool up = false;
-            int actualRubric = 1;
+            short actualRubric = 1;
 
             Console.SetWindowSize(120, 58);
             Console.SetWindowPosition(0, 0);
@@ -38,21 +38,24 @@ namespace SpaceInvader
             titlesLocation[3] = 29;
             titlesLocation[4] = 38;
             titlesLocation[5] = 47;
-
-
             Console.SetWindowPosition(0, 0);
             Console.CursorVisible = false;
 
-            DrawMainMenu(titles);
-            MainRubricWritter(titles, titlesLocation, actualRubric, up);
+            DrawMenu(titles);
+            RubricWritter(titles, titlesLocation, actualRubric, up);
 
 
 
 
             do
             {
+<<<<<<< HEAD
                 
                 MainNavigation(titles, titlesLocation, actualRubric, up);
+=======
+
+                actualRubric = navigation(titles, titlesLocation, actualRubric, up);
+>>>>>>> parent of c6d002d (09.11.2022)
 
             } while (titles[0] != "");
 
@@ -60,6 +63,18 @@ namespace SpaceInvader
 
 
             Console.ReadLine();
+        }
+
+        static void DrawMenu(string[] title)
+        {
+
+            Console.SetCursorPosition(0, 12);
+            for (int a = 1; a < title.Length; a++)
+            {
+                Console.Write(title[a] + "\n\n");
+            }
+            TitleWritter(title[0]);
+
         }
         static void TitleWritter(string title)
         {
@@ -80,21 +95,7 @@ namespace SpaceInvader
             }
         }
 
-
-        //------------------------------------Main Menu------------------------------------//
-        static void DrawMainMenu(string[] title)
-        {
-            Console.Clear();
-            Console.SetCursorPosition(0, 12);
-            for (int a = 1; a < title.Length; a++)
-            {
-                Console.Write(title[a] + "\n\n");
-            }
-            TitleWritter(title[0]);
-
-        }
-
-        static void MainRubricWritter(string[] rubric, short[] localistaion, int actualRubric, bool up)
+        static void RubricWritter(string[] rubric, short[] localistaion, short actualRubric, bool up)
         {
             if (up == true && actualRubric > 0 && actualRubric < 6)
             {
@@ -125,8 +126,7 @@ namespace SpaceInvader
             Console.ForegroundColor = ConsoleColor.White;
 
         }
-
-        static int MainNavigation(string[] titles, short[] titlesLocation, int actualRubric, bool up)
+        static short navigation(string[] titles, short[] titlesLocation, short actualRubric, bool up)
         {
             ConsoleKeyInfo arrow = Console.ReadKey();
 
@@ -138,116 +138,35 @@ namespace SpaceInvader
                     up = true;
                     if (actualRubric <= 0)
                     {
-                        actualRubric = titles.Length;
+                        actualRubric = 5;
                     }
-                    MainRubricWritter(titles, titlesLocation, actualRubric, up);
                     break;
 
                 case ConsoleKey.DownArrow:
                     actualRubric++;
                     up = false;
-                    if (actualRubric >= titles.Length)
+                    if (actualRubric >= 6)
                     {
                         actualRubric = 1;
                     }
-                    MainRubricWritter(titles, titlesLocation, actualRubric, up);
                     break;
                 case ConsoleKey.Enter:
-                    MainMenuActions(actualRubric);
+                    MenuActions(actualRubric);
                     break;
             }
-
-
+            RubricWritter(titles, titlesLocation, actualRubric, up);
 
             return actualRubric;
         }
 
-        static void MainMenuActions(int actualRubric)
+        static void MenuActions(short actualRubric)
         {
-
-            string[] settings = new string[8];
-            settings[0] = "                                                       _____\n                                                      /  ___|\n                                                      \\ `--.  ___  _ __\n                                                       `--. \\/ _ \\| '_ \\\n                                                      /\\__/ / (_) | | | |\n                                                      \\____/ \\___/|_| |_|\n";
-            settings[1] = "                                                         _____       _\n                                                        |  _  |     (_)\n                                                        | | | |_   _ _ \n                                                        | | | | | | | |\n                                                        \\ \\_/ / |_| | |\n                                                         \\___/ \\__,_|_|\n";
-            //settings[2] = "     __\n    / /\n   / /\n  / /\n / /\n/_/\n";
-            settings[2] = "                                                       _   _\n                                                      | \\ | |\n                                                      |  \\| | ___  _ __\n                                                      | . ` |/ _ \\| '_ \\\n                                                      | |\\  | (_) | | | |\n                                                      \\_| \\_/\\___/|_| |_|\n";
-            settings[3] = "                                              ______ _  __ _            _ _\n                                              |  _  (_)/ _(_)          | | |\n                                              | | | |_| |_ _  ___ _   _| | |_ ___\n                                              | | | | |  _| |/ __| | | | | __/ _ \\\n                                              | |/ /| | | | | (__| |_| | | ||  __/\n                                              |___/ |_|_| |_|\\___|\\__,_|_|\\__\\___|\n";
-            settings[4] = "                                               _   _                            _\n                                              | \\ | |                          | |\n                                              |  \\| | ___  _ __ _ __ ___   __ _| |\n                                              | . ` |/ _ \\| '__| '_ ` _ \\ / _` | |\n                                              | |\\  | (_) | |  | | | | | | (_| | |\n                                              \\_| \\_/\\___/|_|  |_| |_| |_|\\__,_|_|\n";
-            settings[5] = "                                                ______ _  __  __ _      _ _\n                                                |  _  (_)/ _|/ _(_)    (_) |\n                                                | | | |_| |_| |_ _  ___ _| | ___\n                                                | | | | |  _|  _| |/ __| | |/ _ \\\n                                                | |/ /| | | | | | | (__| | |  __/\n                                                |___/ |_|_| |_| |_|\\___|_|_|\\___|\n";
-            settings[6] = "                                           ______         _\n                                           | |_/ /_ _  __| | __ ___      ____ _ _ __\n                                           |  __/ _` |/ _` |/ _` \\ \\ /\\ / / _` | '_ \\\n                                           | | | (_| | (_| | (_| |\\ V  V / (_| | | | |\n                                           \\_|  \\__,_|\\__,_|\\__,_| \\_/\\_/ \\__,_|_| |_|\n";
-
-
-            switch (actualRubric)
+            if (actualRubric == 5)
             {
-                case 2:
-                    DrawSettingMenu(settings);
-                    break;
-
-                case 5:
-                    Environment.Exit(0);
-                    break;
-
+                Environment.Exit(0);
 
             }
-
         }
-
-        //------------------------------------Setting Menu------------------------------------//
-
-        public static void DrawSettingMenu(string[] title)
-        {
-            Console.Clear();
-            Console.SetCursorPosition(0, 12);
-            for (int a = 0; a < title.Length; a++)
-            {
-                if (a != 2 && a != 5 && a != 6)
-                {
-                    if(a == 1 || a == 5)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    }
-                    Console.Write(title[a] + "\n\n");
-                    Console.ForegroundColor = ConsoleColor.White;
-
-                }
-            }
-
-        }
-
-
-        static void SettingRubricWritter(string[] rubric, short[] localistaion, int actualRubric, bool up)
-        {
-            if (up == true && actualRubric > 0 && actualRubric < 5)
-            {
-                Console.SetCursorPosition(0, localistaion[actualRubric + 1]);
-                Console.Write(rubric[actualRubric + 1]);
-            }
-            else if (up == false && actualRubric < 6 && actualRubric > 1)
-            {
-                Console.SetCursorPosition(0, localistaion[actualRubric - 1]);
-                Console.Write(rubric[actualRubric - 1]);
-
-            }
-            else if (up == true || actualRubric == 5)
-            {
-                Console.SetCursorPosition(0, localistaion[1]);
-                Console.Write(rubric[1]);
-
-            }
-            else if (up == false || actualRubric == 1)
-            {
-                Console.SetCursorPosition(0, localistaion[5]);
-                Console.Write(rubric[5]);
-            }
-
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.SetCursorPosition(0, localistaion[actualRubric]);
-            Console.Write(rubric[actualRubric]);
-            Console.ForegroundColor = ConsoleColor.White;
-
-        }
-
-
-
 
     }
 
